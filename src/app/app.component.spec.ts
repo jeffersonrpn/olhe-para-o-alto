9cff6e11e2ee849c8bf1ce5bce9cf77e39dd79eb
Jefferson Neves <jefferson.rpn@gmail.com>
2017-04-18 17:10:44 -0300

    Add dependencies to tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import {NavComponent} from "./nav/nav.component";
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        NavComponent
       ],
     }).compileComponents();
   }));
